ce8ec4ec05e3254c62fedab5e83ee08bf1ec944a
Abdlazz0 <154979182+Abdlazz0@users.noreply.github.com>
2026-01-07 14:49:20 +0700

    fixing route redirect /admin

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use App\Services\FonnteService;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', function () {
+    return redirect('/admin');
+});
 //Route::get('/', function () {
 //    return view('welcome');
 //});
