b3291df71737291ed52faafa7a789082a137fb53
Abdlazz0 <154979182+Abdlazz0@users.noreply.github.com>
2026-01-21 11:25:21 +0700

    fixing

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
+        User::firstOrCreate([
             'name' => 'Super Admin',
             'email' => 'admin@admin.com',
             'password' => Hash::make('123'),
